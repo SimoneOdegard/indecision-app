e58213971132b306ee24e1ab993ea749a9d6cdb6
Simone Odegard <simone.odegard@gmail.com>
2022-07-01 11:54:37 -0700

    created indecision app. following react course for review

---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log('app.js is running!');
+
+// JSX - JavaScript XML
+let template = <h1>Indecision App</h1>;
+let appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
