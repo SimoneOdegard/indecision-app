310d30893f1957ffd04526c0616e4a4862c54dac
Simone Odegard <simone.odegard@gmail.com>
2022-07-02 16:13:54 -0700

    completed course videos 10 and 12 from section 3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,73 @@
 
 console.log('app.js is running!');
 
+// ** ======================================== ** //
+// **               Challenge  2               ** //
+// 1. create app object title/subtitle
+// A. use title/subtitle in the template
+// B. render template
+// ** ======================================== ** //
+
 // JSX - JavaScript XML
+// need wrapper div
+var app = {
+  title: 'Indecision App',
+  subtitle: 'A practice app for reviewing React'
+};
+
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  )
 );
+
+// ** ======================================== ** //
+// **               Challenge  1               ** //
+// 1. create a templateTwo var JSX expression
+// A. root div
+//      h1 -> your name
+//      p -> Age: 32
+//      p -> Location: Renton, WA
+// B. render templateTwo instead of template
+// ** ======================================== ** //
+
+var user = {
+  name: 'Monie',
+  age: 32,
+  location: 'Renton'
+};
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Location: ',
+    user.location
+  )
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,7 +1,51 @@
 console.log('app.js is running!');
 
+// ** ======================================== ** //
+// **               Challenge  2               ** //
+// 1. create app object title/subtitle
+// A. use title/subtitle in the template
+// B. render template
+// ** ======================================== ** //
+
 // JSX - JavaScript XML
-let template = <h1>Indecision App</h1>;
-let appRoot = document.getElementById('app');
+// need wrapper div
+const app = {
+  title: 'Indecision App',
+  subtitle: 'A practice app for reviewing React'
+}
+
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+  </div>
+);
+
+// ** ======================================== ** //
+// **               Challenge  1               ** //
+// 1. create a templateTwo var JSX expression
+// A. root div
+//      h1 -> your name
+//      p -> Age: 32
+//      p -> Location: Renton, WA
+// B. render templateTwo instead of template
+// ** ======================================== ** //
+
+const user = {
+  name: 'Monie',
+  age: 32,
+  location: 'Renton'
+};
+
+const templateTwo = (
+  <div>
+    <h1>{user.name}</h1>
+    <p>Age: {user.age}</p>
+    <p>Location: {user.location}</p> 
+  </div>
+);
+
+
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
